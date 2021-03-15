421edcdeafed5825bf56140292abdfbf0e0b78ac
Al <al@openkoi.com>
2021-03-15 18:05:46 -0300

    ff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { joinKoi } = require('koi-logs');
 var app = new Express ();
 
 // add koi tasks
-joinKoi(app);
+joinKoi(app, '/home/al/');
 
 // start the server listener
 app.listen(process.env.PORT || 3000, () => {
